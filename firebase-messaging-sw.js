ce41f06522ee293ea3dea49d24082f9ec32bc046
unicationedu <unication.edu@gmail.com>
2026-06-02 10:31:54 +0900

    Add files via upload

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,21 @@
+importScripts('https://www.gstatic.com/firebasejs/10.8.1/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.8.1/firebase-messaging-compat.js');
+
+firebase.initializeApp({
+     apiKey: "AIzaSyCQe2H3YBOve8rJtGakTItqeYFIIAXxieM",
+  authDomain: "unication-9af0d.firebaseapp.com",
+  projectId: "unication-9af0d",
+  storageBucket: "unication-9af0d.firebasestorage.app",
+  messagingSenderId: "365505602833",
+  appId: "1:365505602833:web:3cec281f3d33967b2198b0"
+};
+
+const messaging = firebase.messaging();
+messaging.onBackgroundMessage((payload) => {
+    const { title, body } = payload.notification || {};
+    if (!title) return;
+    self.registration.showNotification(title, {
+        body: body || '',
+        icon: '/icon-192.png'
+    });
+});
